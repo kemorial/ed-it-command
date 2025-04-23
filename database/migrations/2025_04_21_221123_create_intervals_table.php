60adba0a62a7d492c5afa6b318f6f89e7c2743d0
kemorial <chubukk@inbox.ru>
2025-04-23 12:08:13 +0300

    modeld and migrations without timestamps

--- database/migrations/2025_04_21_221123_create_intervals_table.php
+++ database/migrations/2025_04_21_221123_create_intervals_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->integer('start');
             $table->integer('end')->nullable()->default(null);
+            $table->index('start');
+            $table->index('end');
+            $table->index(['start', 'end']);
         });
     }
 
